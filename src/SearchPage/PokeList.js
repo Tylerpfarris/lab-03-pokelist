16a3d3c8710b26c34747306273dce1ad01fa9667
Tyler Farris <Tylerpfarris@gmail.com>
2021-02-15 13:52:30 -0800

    all feature working

--- src/SearchPage/PokeList.js
+++ src/SearchPage/PokeList.js
@@ -7,7 +7,7 @@ export default class PokeList extends Component {
         return (
             <section className={style.pokeList}>
                 {this.props.pokeArray.map(pokemonObj =>
-                    <PokeItem pokemon = {pokemonObj.pokemon} type = {pokemonObj.type_1} weight = {pokemonObj.weight} height = {pokemonObj.height} key = {pokemonObj._id} pokeImage = {pokemonObj.url_image} attack = {pokemonObj.attack} defense = {pokemonObj.defense} />
+                    <PokeItem pokemon = {pokemonObj.pokemon} type = {pokemonObj.type_1} weight = {pokemonObj.weight} height = {pokemonObj.height} key = {pokemonObj._id} pokeImage = {pokemonObj.url_image} attack = {pokemonObj.attack} defense = {pokemonObj.defense}  />
                 )}
             </section>
         )
